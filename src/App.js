25581bf4817168e7276451e138ac42f6583f2bd5
conrad96 <conrad.mugisha@gmail.com>
2021-05-09 21:23:40 +0300

    code to style phonebook page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app__header">
+        <h2>PhoneBook</h2>
       </header>
+      <div class="app__searchRecord">
+        <form>
+          <input type="text" placeholder="Search..." className="form-control" />
+        </form>
+      </div>
+      <div className="app__contactsList">  
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Contacts</th>
+            </tr>
+          </thead>          
+        </Table>
+      </div>
     </div>
   );
 }
